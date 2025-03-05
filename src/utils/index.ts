d7314a20b044322029e161b9e7267de5826c6fb2
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-05 01:45:44 +0000

    add typewriter function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,13 @@ export const safeAssign: <T extends object>(
   target: T,
   source: Partial<T>
 ) => T = Object.assign;
+
+export const wait = (ms: number) =>
+  new Promise<void>((resolve) => setTimeout(resolve, ms));
+
+export async function* typewriter(text: string, delay = 50) {
+  for (const char of text) {
+    yield char;
+    await wait(delay);
+  }
+}
