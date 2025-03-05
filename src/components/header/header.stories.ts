edd06c371c0bc6e5f32222d8b208f85b4beb1ca9
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-05 02:32:13 +0000

    add storybook for page header

---
+++ src/components/header/header.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import "./header";
+
+const meta = {
+  title: "Header",
+  tags: ["autodocs"],
+  component: "page-header",
+  args: {
+    innerText: "Page header",
+  },
+} satisfies Meta<HTMLElementTagNameMap["page-header"]>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
