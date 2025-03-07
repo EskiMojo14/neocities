04462f6faf0b46ea940ab39fa12fe204ae86b009
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-07 02:08:15 +0000

    restructure sidebar items

--- src/components/sidebar/sidebar.ts
+++ src/components/sidebar/sidebar.ts
@@ -1,8 +1,8 @@
 import { LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { html } from "lit-html";
+import { html, type TemplateResult } from "lit-html";
 import { ifDefined } from "lit-html/directives/if-defined.js";
-import { map } from "lit-html/directives/map.js";
+import { repeat } from "lit-html/directives/repeat.js";
 import type { WithOptional } from "../../utils/index.ts";
 import { isActiveLink, clsx } from "../../utils/lit.ts";
 import "../symbol/symbol.ts";
@@ -11,7 +11,7 @@ import base from "../../styles/base.css" with { type: "css" };
 import typography from "../../styles/typography.css" with { type: "css" };
 
 interface SidebarItem {
-  type?: "item";
+  type: "item";
   label: string;
   href: string;
   icon: string;
@@ -20,11 +20,14 @@ interface SidebarItem {
 type GroupIconUnion =
   | {
       childIcon: string;
-      items: Array<WithOptional<SidebarItem, "icon">>;
+      children: Record<
+        string,
+        WithOptional<SidebarItem, "icon"> | SidebarGroup
+      >;
     }
   | {
       childIcon?: never;
-      items: Array<SidebarItem>;
+      children: Record<string, SidebarItem | SidebarGroup>;
     };
 
 type SidebarGroup = GroupIconUnion & {
@@ -34,35 +37,80 @@ type SidebarGroup = GroupIconUnion & {
   icon: string;
 };
 
-const sidebarItems: Array<SidebarItem | SidebarGroup> = [
-  {
+const sidebarItems: Record<string, SidebarItem | SidebarGroup> = {
+  "/": {
+    type: "item",
     label: "Home",
     href: "/",
     icon: "home",
   },
-  {
+  "/about": {
+    type: "item",
     label: "About",
     href: "/about/",
     icon: "info",
   },
-  {
+  "/blog": {
+    type: "group",
     label: "Blog",
     href: "/blog/",
-    type: "group",
     icon: "newsmode",
     childIcon: "article",
-    items: [
-      {
+    children: {
+      "/post-1": {
+        type: "item",
         label: "Post 1",
         href: "/blog/post-1/",
       },
-      {
+      "/post-2": {
+        type: "item",
         label: "Post 2",
         href: "/blog/post-2/",
       },
-    ],
+    },
   },
-];
+};
+
+function renderSidebarItem(item: SidebarItem, currentRoute: string) {
+  return html`<li>
+    <a
+      href=${item.href}
+      class=${clsx("subtitle1", {
+        current: isActiveLink(item.href, currentRoute),
+      })}
+    >
+      <material-symbol>${item.icon}</material-symbol>
+      ${item.label}
+    </a>
+  </li>`;
+}
+
+function renderSidebarGroup(group: SidebarGroup, currentRoute: string) {
+  return html`<li class="group">
+    <a
+      href=${ifDefined(group.href)}
+      class=${clsx("subtitle1", {
+        current: isActiveLink(group.href, currentRoute),
+      })}
+    >
+      <material-symbol>${group.icon}</material-symbol>
+      ${group.label}</a
+    >
+    <ul>
+      ${repeat(
+        Object.values(group.children),
+        (item) => item.href,
+        (item): TemplateResult<1> =>
+          item.type === "item"
+            ? renderSidebarItem(
+                { ...item, icon: item.icon ?? group.childIcon ?? "" },
+                currentRoute,
+              )
+            : renderSidebarGroup(item, currentRoute),
+      )}
+    </ul>
+  </li>`;
+}
 
 @customElement("sidebar-nav")
 export default class Sidebar extends LitElement {
@@ -75,60 +123,14 @@ export default class Sidebar extends LitElement {
     return html`
       <nav>
         <ul>
-          ${map(sidebarItems, (item) => {
-            if (item.type === "group") {
-              return html`
-                <li class="group">
-                  <a
-                    href=${ifDefined(item.href)}
-                    class=${clsx("subtitle1", {
-                      current: isActiveLink(item.href, this.currentRoute),
-                    })}
-                  >
-                    <material-symbol>${item.icon}</material-symbol>
-                    ${item.label}
-                  </a>
-                  <ul>
-                    ${map(
-                      item.items,
-                      (subItem) => html`
-                        <li>
-                          <a
-                            href=${subItem.href}
-                            class=${clsx("subtitle2", {
-                              current: isActiveLink(
-                                subItem.href,
-                                this.currentRoute,
-                              ),
-                            })}
-                          >
-                            <material-symbol>
-                              ${subItem.icon ?? item.childIcon}
-                            </material-symbol>
-                            ${subItem.label}
-                          </a>
-                        </li>
-                      `,
-                    )}
-                  </ul>
-                </li>
-              `;
-            } else {
-              return html`
-                <li>
-                  <a
-                    href=${item.href}
-                    class=${clsx("subtitle1", {
-                      current: isActiveLink(item.href, this.currentRoute),
-                    })}
-                  >
-                    <material-symbol>${item.icon}</material-symbol>
-                    ${item.label}
-                  </a>
-                </li>
-              `;
-            }
-          })}
+          ${repeat(
+            Object.values(sidebarItems),
+            (item) => item.href,
+            (item) =>
+              item.type === "item"
+                ? renderSidebarItem(item, this.currentRoute)
+                : renderSidebarGroup(item, this.currentRoute),
+          )}
         </ul>
       </nav>
     `;
