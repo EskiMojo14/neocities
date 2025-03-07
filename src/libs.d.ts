0776a14b1845603de385fe12dea9ed3b85f291e5
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-07 01:18:22 +0000

    move CSS plugin to separate dir

--- src/libs.d.ts
+++ src/libs.d.ts
@@ -4,3 +4,34 @@ declare module "*.module.css" {
 }
 
 declare module "*.css";
+
+declare module "@greenwood/cli/src/lifecycles/config.js" {
+  import type { UserConfig } from "vite";
+  export function readAndMergeConfig(): Promise<UserConfig>;
+}
+
+interface GreenwoodContext {}
+
+declare module "@greenwood/cli/src/lifecycles/context.js" {
+  import type { UserConfig } from "vite";
+  export function initContext(opts: {
+    config: UserConfig;
+  }): Promise<GreenwoodContext>;
+}
+
+declare module "@greenwood/cli/src/plugins/resource/plugin-standard-css.js" {
+  import type { UserConfig } from "vite";
+
+  export const greenwoodPluginStandardCss: {
+    provider: (compilation: {
+      context: GreenwoodContext;
+      config: UserConfig;
+    }) => {
+      intercept: (
+        url: URL,
+        request: Request,
+        response: Response
+      ) => Promise<Response>;
+    };
+  };
+}
