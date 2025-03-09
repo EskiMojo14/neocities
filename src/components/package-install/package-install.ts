c8d73973d891a8414f1cfa6e1ac7652842b73e19
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-09 00:22:33 +0000

    simplify package install component

--- src/components/package-install/package-install.ts
+++ src/components/package-install/package-install.ts
@@ -1,16 +1,7 @@
 import { LitElement, unsafeCSS } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 import { html } from "lit-html";
 import base from "../../styles/base.css?type=raw";
-
-const installCommands = {
-  npm: "install",
-  yarn: "add",
-  pnpm: "add",
-  bun: "add",
-} satisfies Record<string, string>;
-
-type PackageManager = keyof typeof installCommands;
 
 @customElement("package-install")
 export default class PackageInstall extends LitElement {
@@ -19,16 +10,10 @@ export default class PackageInstall extends LitElement {
   @property({ type: String, attribute: "dev-dep" })
   devDep = "";
 
-  @state()
-  packageManager: PackageManager = "pnpm";
-
   render() {
     const { devDep } = this;
     return html`
-      <code
-        >${this.packageManager} ${installCommands[this.packageManager]}
-        ${devDep === "true" ? "-D" : ""} <slot></slot
-      ></code>
+      <code>pnpm add ${devDep === "true" ? "-D" : ""} <slot></slot></code>
     `;
   }
 }
