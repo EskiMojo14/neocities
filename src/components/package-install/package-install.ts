40c847b2da3c1501430b664a5616cd6de4f60db0
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-09 03:21:01 +0000

    switch to monaspace font

--- src/components/package-install/package-install.ts
+++ src/components/package-install/package-install.ts
@@ -1,11 +1,13 @@
 import { LitElement, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { html } from "lit-html";
+import { when } from "lit-html/directives/when.js";
+import dracula from "../../../node_modules/dracula-prism/dist/css/dracula-prism.css?type=raw";
 import base from "../../styles/base.css?type=raw";
 
 @customElement("package-install")
 export default class PackageInstall extends LitElement {
-  static styles = [unsafeCSS(base)];
+  static styles = [unsafeCSS(dracula), unsafeCSS(base)];
 
   @property({ type: String, attribute: "dev-dep" })
   devDep = "";
@@ -13,7 +15,12 @@ export default class PackageInstall extends LitElement {
   render() {
     const { devDep } = this;
     return html`
-      <code>pnpm add ${devDep === "true" ? "-D" : ""} <slot></slot></code>
+      <pre
+        class="language-bash"
+      ><code class="language-bash"><span class="token function">pnpm</span> <span class="token function">add</span> ${when(
+        devDep === "true",
+        () => html`<span class="token parameter variable">-D</span> `,
+      )}<slot></slot></code></pre>
     `;
   }
 }
