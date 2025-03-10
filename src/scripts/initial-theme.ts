c43c4cccb726f687fd77134b1817371eb291e0ae
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-10 15:47:13 +0000

    handle initial theme in SSR

--- src/scripts/initial-theme.ts
+++ src/scripts/initial-theme.ts
@@ -1,7 +1,10 @@
 import * as v from "valibot";
+import { assert } from "../utils/index.ts";
 const themeSchema = v.picklist(["light", "dark"]);
 
 try {
+  // don't run in SSR
+  assert(typeof window !== "undefined");
   document.documentElement.dataset.theme = v.parse(
     themeSchema,
     localStorage.getItem("theme"),
