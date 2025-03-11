eb0f52ebafc28ee86415316f1e77d28f174a6fcd
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-11 13:31:09 +0000

    update to greenwood alpha and use built in TS support

--- greenwood.config.ts
+++ greenwood.config.ts
@@ -1,20 +1,14 @@
+import type { Config } from "@greenwood/cli";
 import { greenwoodPluginImportRaw } from "@greenwood/plugin-import-raw";
 import { greenwoodPluginRendererLit } from "@greenwood/plugin-renderer-lit";
-import { greenwoodPluginTypeScript } from "@greenwood/plugin-typescript";
 
 export default {
   prerender: true,
   activeContent: true,
   isolation: true,
-  plugins: [
-    greenwoodPluginTypeScript({
-      servePage: false,
-      extendConfig: true,
-    }),
-    greenwoodPluginRendererLit(),
-    greenwoodPluginImportRaw(),
-  ],
+  plugins: [greenwoodPluginRendererLit(), greenwoodPluginImportRaw()],
   markdown: {
     plugins: ["@mapbox/rehype-prism"],
   },
-};
+  useTsc: true,
+} satisfies Config;
