d5275f7b5d62064238fb8ef4d6f9847da467a541
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-26 01:01:39 +0000

    create tags list component

--- src/components/pkg-card/pkg-card.ts
+++ src/components/pkg-card/pkg-card.ts
@@ -1,10 +1,10 @@
 import { getContentByRoute } from "@greenwood/cli/src/data/client.js";
 import { html, LitElement, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { when } from "lit/directives/when.js";
 import base from "../../styles/utility/baseline.css?type=raw";
 import typography from "../../styles/utility/typography.css?type=raw";
 import "../pkg-info/pkg-info.ts";
+import "../tags/tags.ts";
 import pkgCard from "./pkg-card.css?type=raw";
 
 const pkgChildIcon = String(
@@ -51,20 +51,7 @@ export default class PkgCard extends LitElement {
           <material-symbol aria-hidden="true">${icon}</material-symbol>
         </a>
         <p class="subtitle2">${description}</p>
-        ${when(
-          this.tags.length,
-          () =>
-            html`<p class="caption">
-              Tags:
-              ${this.tags.map(
-                (tag, idx) =>
-                  html`<a href="/packages/tags/${tag}/">${tag}</a>${idx <
-                    this.tags.length - 1
-                      ? ", "
-                      : ""}`,
-              )}
-            </p>`,
-        )}
+        <tags-list path="packages" .tags=${this.tags}></tags-list>
         <pkg-info pkg=${pkg} repo=${repo} docs=${docs}></pkg-info>
       </div>
     `;
