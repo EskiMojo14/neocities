28988f17decc93159c1f5eeed19224264c844b3f
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-31 11:34:19 +0100

    add custom serializer for valibot errors

--- src/utils/valibot.node.test.ts
+++ src/utils/valibot.node.test.ts
@@ -2,6 +2,19 @@ import * as v from "valibot";
 import { describe, expect, it } from "vitest";
 import * as vUtils from "./valibot.ts";
 
+expect.addSnapshotSerializer({
+  test: (value) => value instanceof v.ValiError,
+  serialize: (
+    valiError: v.ValiError<v.GenericSchema>,
+    config,
+    indentation,
+    depth,
+    refs,
+    printer,
+  ) =>
+    `[ValiError: ${valiError.message}]: ${printer(valiError.issues, config, indentation, depth, refs)}`,
+});
+
 describe("valibot utils", () => {
   describe("formDataShape", () => {
     it("should parse a valid FormData", () => {
@@ -27,9 +40,24 @@ describe("valibot utils", () => {
           }),
           null,
         ),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Invalid type: Expected FormData but received null]`,
-      );
+      ).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Invalid type: Expected FormData but received null]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": "FormData",
+            "input": null,
+            "issues": undefined,
+            "kind": "schema",
+            "lang": undefined,
+            "message": "Invalid type: Expected FormData but received null",
+            "path": undefined,
+            "received": "null",
+            "requirement": undefined,
+            "type": "instance",
+          },
+        ]
+      `);
     });
 
     it("should throw an error if the FormData does not match the schema", () => {
@@ -42,9 +70,34 @@ describe("valibot utils", () => {
           }),
           formData,
         );
-      }).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Invalid type: Expected File but received "bar"]`,
-      );
+      }).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Invalid type: Expected File but received "bar"]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": "File",
+            "input": "bar",
+            "issues": undefined,
+            "kind": "schema",
+            "lang": undefined,
+            "message": "Invalid type: Expected File but received "bar"",
+            "path": [
+              {
+                "input": {
+                  "foo": "bar",
+                },
+                "key": "foo",
+                "origin": "value",
+                "type": "object",
+                "value": "bar",
+              },
+            ],
+            "received": ""bar"",
+            "requirement": undefined,
+            "type": "file",
+          },
+        ]
+      `);
     });
   });
   describe("coerceNumber", () => {
@@ -53,19 +106,47 @@ describe("valibot utils", () => {
     });
 
     it("should throw an error if the string is not a number", () => {
-      expect(() =>
-        v.parse(vUtils.coerceNumber, "a"),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Invalid type: Expected number but received NaN]`,
-      );
+      expect(() => v.parse(vUtils.coerceNumber, "a"))
+        .toThrowErrorMatchingInlineSnapshot(`
+          [ValiError: Invalid type: Expected number but received NaN]: [
+            {
+              "abortEarly": undefined,
+              "abortPipeEarly": undefined,
+              "expected": "number",
+              "input": NaN,
+              "issues": undefined,
+              "kind": "schema",
+              "lang": undefined,
+              "message": "Invalid type: Expected number but received NaN",
+              "path": undefined,
+              "received": "NaN",
+              "requirement": undefined,
+              "type": "number",
+            },
+          ]
+        `);
     });
 
     it("should throw an error if not a string", () => {
-      expect(() =>
-        v.parse(vUtils.coerceNumber, null),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Invalid type: Expected string but received null]`,
-      );
+      expect(() => v.parse(vUtils.coerceNumber, null))
+        .toThrowErrorMatchingInlineSnapshot(`
+          [ValiError: Invalid type: Expected string but received null]: [
+            {
+              "abortEarly": undefined,
+              "abortPipeEarly": undefined,
+              "expected": "string",
+              "input": null,
+              "issues": undefined,
+              "kind": "schema",
+              "lang": undefined,
+              "message": "Invalid type: Expected string but received null",
+              "path": undefined,
+              "received": "null",
+              "requirement": undefined,
+              "type": "string",
+            },
+          ]
+        `);
     });
   });
   describe("json", () => {
@@ -78,25 +159,79 @@ describe("valibot utils", () => {
     it("should throw an error if the JSON is invalid", () => {
       expect(() =>
         v.parse(vUtils.json(v.object({ foo: v.string() })), '{"foo":"bar"'),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Expected ',' or '}' after property value in JSON at position 12 (line 1 column 13)]`,
-      );
+      ).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Expected ',' or '}' after property value in JSON at position 12 (line 1 column 13)]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": null,
+            "input": "{"foo":"bar"",
+            "issues": undefined,
+            "kind": "transformation",
+            "lang": undefined,
+            "message": "Expected ',' or '}' after property value in JSON at position 12 (line 1 column 13)",
+            "path": undefined,
+            "received": ""{"foo":"bar""",
+            "requirement": undefined,
+            "type": "raw_transform",
+          },
+        ]
+      `);
     });
 
     it("should throw an error if the JSON is not a string", () => {
-      expect(() =>
-        v.parse(vUtils.json(v.object({ foo: v.string() })), null),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Invalid type: Expected string but received null]`,
-      );
+      expect(() => v.parse(vUtils.json(v.object({ foo: v.string() })), null))
+        .toThrowErrorMatchingInlineSnapshot(`
+          [ValiError: Invalid type: Expected string but received null]: [
+            {
+              "abortEarly": undefined,
+              "abortPipeEarly": undefined,
+              "expected": "string",
+              "input": null,
+              "issues": undefined,
+              "kind": "schema",
+              "lang": undefined,
+              "message": "Invalid type: Expected string but received null",
+              "path": undefined,
+              "received": "null",
+              "requirement": undefined,
+              "type": "string",
+            },
+          ]
+        `);
     });
 
     it("should throw an error if the JSON does not match the schema", () => {
       expect(() =>
         v.parse(vUtils.json(v.object({ foo: v.string() })), '{"foo":1}'),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[ValiError: Invalid type: Expected string but received 1]`,
-      );
+      ).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Invalid type: Expected string but received 1]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": "string",
+            "input": 1,
+            "issues": undefined,
+            "kind": "schema",
+            "lang": undefined,
+            "message": "Invalid type: Expected string but received 1",
+            "path": [
+              {
+                "input": {
+                  "foo": 1,
+                },
+                "key": "foo",
+                "origin": "value",
+                "type": "object",
+                "value": 1,
+              },
+            ],
+            "received": "1",
+            "requirement": undefined,
+            "type": "string",
+          },
+        ]
+      `);
     });
     it("should parse a valid JSON string with a reviver", () => {
       expect(
@@ -107,6 +242,205 @@ describe("valibot utils", () => {
           '{"foo":"bar"}',
         ),
       ).toEqual({ foo: "BAR" });
+    });
+  });
+  describe("maybeJson", () => {
+    it("should parse a valid JSON string", () => {
+      expect(
+        v.parse(
+          vUtils.maybeJson(v.object({ foo: v.string() })),
+          '{"foo":"bar"}',
+        ),
+      ).toEqual({ foo: "bar" });
+    });
+
+    it("should parse a valid object", () => {
+      expect(
+        v.parse(vUtils.maybeJson(v.object({ foo: v.string() })), {
+          foo: "bar",
+        }),
+      ).toEqual({ foo: "bar" });
+    });
+
+    it("should throw an error if the JSON is invalid", () => {
+      expect(() =>
+        v.parse(
+          vUtils.maybeJson(v.object({ foo: v.string() })),
+          '{"foo":"bar"',
+        ),
+      ).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Invalid type: Expected (Object | string) but received "{"foo":"bar""]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": "(Object | string)",
+            "input": "{"foo":"bar"",
+            "issues": [
+              {
+                "abortEarly": undefined,
+                "abortPipeEarly": undefined,
+                "expected": "Object",
+                "input": "{"foo":"bar"",
+                "issues": undefined,
+                "kind": "schema",
+                "lang": undefined,
+                "message": "Invalid type: Expected Object but received "{"foo":"bar""",
+                "path": undefined,
+                "received": ""{"foo":"bar""",
+                "requirement": undefined,
+                "type": "object",
+              },
+              {
+                "abortEarly": undefined,
+                "abortPipeEarly": undefined,
+                "expected": null,
+                "input": "{"foo":"bar"",
+                "issues": undefined,
+                "kind": "transformation",
+                "lang": undefined,
+                "message": "Expected ',' or '}' after property value in JSON at position 12 (line 1 column 13)",
+                "path": undefined,
+                "received": ""{"foo":"bar""",
+                "requirement": undefined,
+                "type": "raw_transform",
+              },
+            ],
+            "kind": "schema",
+            "lang": undefined,
+            "message": "Invalid type: Expected (Object | string) but received "{"foo":"bar""",
+            "path": undefined,
+            "received": ""{"foo":"bar""",
+            "requirement": undefined,
+            "type": "union",
+          },
+        ]
+      `);
+    });
+    it("should throw an error if the JSON does not match the schema", () => {
+      expect(() =>
+        v.parse(vUtils.maybeJson(v.object({ foo: v.string() })), '{"foo":1}'),
+      ).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Invalid type: Expected (Object | string) but received "{"foo":1}"]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": "(Object | string)",
+            "input": "{"foo":1}",
+            "issues": [
+              {
+                "abortEarly": undefined,
+                "abortPipeEarly": undefined,
+                "expected": "Object",
+                "input": "{"foo":1}",
+                "issues": undefined,
+                "kind": "schema",
+                "lang": undefined,
+                "message": "Invalid type: Expected Object but received "{"foo":1}"",
+                "path": undefined,
+                "received": ""{"foo":1}"",
+                "requirement": undefined,
+                "type": "object",
+              },
+              {
+                "abortEarly": undefined,
+                "abortPipeEarly": undefined,
+                "expected": "string",
+                "input": 1,
+                "issues": undefined,
+                "kind": "schema",
+                "lang": undefined,
+                "message": "Invalid type: Expected string but received 1",
+                "path": [
+                  {
+                    "input": {
+                      "foo": 1,
+                    },
+                    "key": "foo",
+                    "origin": "value",
+                    "type": "object",
+                    "value": 1,
+                  },
+                ],
+                "received": "1",
+                "requirement": undefined,
+                "type": "string",
+              },
+            ],
+            "kind": "schema",
+            "lang": undefined,
+            "message": "Invalid type: Expected (Object | string) but received "{"foo":1}"",
+            "path": undefined,
+            "received": ""{"foo":1}"",
+            "requirement": undefined,
+            "type": "union",
+          },
+        ]
+      `);
+    });
+    it("should throw an error if the object does not match the schema", () => {
+      expect(() =>
+        v.parse(vUtils.maybeJson(v.object({ foo: v.string() })), { foo: 1 }),
+      ).toThrowErrorMatchingInlineSnapshot(`
+        [ValiError: Invalid type: Expected (Object | string) but received Object]: [
+          {
+            "abortEarly": undefined,
+            "abortPipeEarly": undefined,
+            "expected": "(Object | string)",
+            "input": {
+              "foo": 1,
+            },
+            "issues": [
+              {
+                "abortEarly": undefined,
+                "abortPipeEarly": undefined,
+                "expected": "string",
+                "input": 1,
+                "issues": undefined,
+                "kind": "schema",
+                "lang": undefined,
+                "message": "Invalid type: Expected string but received 1",
+                "path": [
+                  {
+                    "input": {
+                      "foo": 1,
+                    },
+                    "key": "foo",
+                    "origin": "value",
+                    "type": "object",
+                    "value": 1,
+                  },
+                ],
+                "received": "1",
+                "requirement": undefined,
+                "type": "string",
+              },
+              {
+                "abortEarly": undefined,
+                "abortPipeEarly": undefined,
+                "expected": "string",
+                "input": {
+                  "foo": 1,
+                },
+                "issues": undefined,
+                "kind": "schema",
+                "lang": undefined,
+                "message": "Invalid type: Expected string but received Object",
+                "path": undefined,
+                "received": "Object",
+                "requirement": undefined,
+                "type": "string",
+              },
+            ],
+            "kind": "schema",
+            "lang": undefined,
+            "message": "Invalid type: Expected (Object | string) but received Object",
+            "path": undefined,
+            "received": "Object",
+            "requirement": undefined,
+            "type": "union",
+          },
+        ]
+      `);
     });
   });
 });
