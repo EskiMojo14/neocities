a4c6774026875581eafe8850aa8648e1732f19cd
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-31 11:42:00 +0100

    fix case toggle test

--- src/vite/setup.browser.ts
+++ src/vite/setup.browser.ts
@@ -1,7 +1,11 @@
 import "mix-n-matchers/vitest";
 import { expect } from "vitest";
 import "vitest-browser-lit";
-import { pkgManagerSchema, themeSchema } from "../constants/prefs.ts";
+import {
+  caseSchema,
+  pkgManagerSchema,
+  themeSchema,
+} from "../constants/prefs.ts";
 import { toHaveData } from "./matchers/to-have-data.ts";
 import { toHaveFocus } from "./matchers/to-have-focus.ts";
 import "../styles/global.css";
@@ -9,6 +13,7 @@ import "../styles/global.css";
 localStorage.clear();
 document.documentElement.dataset.theme = themeSchema.fallback;
 document.documentElement.dataset.pm = pkgManagerSchema.fallback;
+document.documentElement.dataset.case = caseSchema.fallback;
 
 expect.extend({
   // replace jest-dom matcher with one that supports shadow roots
