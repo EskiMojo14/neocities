0b945ed7d1b277ced5da61675de2c21397c79d8a
Ben Durrant <ben.durrant@marketdojo.com>
2025-03-31 13:26:55 +0100

    improve type safety of pref meta

--- src/constants/prefs.ts
+++ src/constants/prefs.ts
@@ -1,59 +1,54 @@
 import * as v from "valibot";
-import type { Compute } from "../utils/index.ts";
 
-function createPref<
-  Schema extends v.SchemaWithFallback<v.GenericSchema<string>, string>,
-  Meta extends Record<v.InferOutput<Schema>, unknown>,
-  Config extends {
-    dataKey: string;
-    storageKey?: string;
-  },
->(
-  schema: Schema,
-  meta: Meta,
-  config: Config,
-): Compute<
-  Config & {
-    storageKey: string;
-    schema: Schema;
-    fallback: v.InferFallback<Schema>;
-    meta: Meta;
-  }
-> {
-  return {
+const createPref =
+  <Meta>() =>
+  <const Opts extends ReadonlyArray<string>, Fallback extends Opts[number]>(
+    options: Opts,
+    fallback: Fallback,
+    {
+      dataKey,
+      storageKey = dataKey,
+      ...config
+    }: {
+      meta: Record<Opts[number], Meta>;
+      dataKey: string;
+      storageKey?: string;
+    },
+  ) => ({
+    schema: v.fallback(v.picklist(options), fallback),
+    fallback,
+    dataKey,
+    storageKey,
     ...config,
-    storageKey: config.storageKey ?? config.dataKey,
-    schema,
-    fallback: v.getFallback(schema),
-    meta,
-  };
-}
+  });
 
-export const pkgManagerPref = createPref(
-  v.fallback(v.picklist(["npm", "pnpm", "yarn", "bun"]), "pnpm"),
-  {
-    npm: { install: "install" },
-    pnpm: { install: "add" },
-    yarn: { install: "add" },
-    bun: { install: "add" },
-  },
+export const pkgManagerPref = createPref<{ install: string }>()(
+  ["npm", "pnpm", "yarn", "bun"],
+  "pnpm",
   {
     dataKey: "pm",
     storageKey: "packageManager",
+    meta: {
+      npm: { install: "install" },
+      pnpm: { install: "add" },
+      yarn: { install: "add" },
+      bun: { install: "add" },
+    },
   },
 );
 
 export type PackageManager = v.InferOutput<typeof pkgManagerPref.schema>;
 
-export const themePref = createPref(
-  v.fallback(v.picklist(["system", "light", "dark"]), "system"),
-  {
-    system: { icon: "routine" },
-    light: { icon: "light_mode" },
-    dark: { icon: "dark_mode" },
-  },
+export const themePref = createPref<{ icon: string }>()(
+  ["system", "light", "dark"],
+  "system",
   {
     dataKey: "theme",
+    meta: {
+      system: { icon: "routine" },
+      light: { icon: "light_mode" },
+      dark: { icon: "dark_mode" },
+    },
   },
 );
 
@@ -63,14 +58,15 @@ export const dirSchema = v.fallback(v.picklist(["auto", "ltr", "rtl"]), "auto");
 
 export type Dir = v.InferOutput<typeof dirSchema>;
 
-export const casePref = createPref(
-  v.fallback(v.picklist(["lower", "normal"]), "lower"),
-  {
-    lower: { icon: "lowercase" },
-    normal: { icon: "match_case" },
-  },
+export const casePref = createPref<{ icon: string }>()(
+  ["lower", "normal"],
+  "lower",
   {
     dataKey: "case",
+    meta: {
+      lower: { icon: "lowercase" },
+      normal: { icon: "match_case" },
+    },
   },
 );
 
