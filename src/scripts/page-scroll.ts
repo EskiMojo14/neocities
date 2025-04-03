37956e2d98cf06a468b27afa639720a78b3cbd6a
Ben Durrant <ben.durrant@marketdojo.com>
2025-04-03 11:32:14 +0100

    fix scroll script in ssr

--- src/scripts/page-scroll.ts
+++ src/scripts/page-scroll.ts
@@ -1,16 +1,22 @@
 import { ScrollStateEvent } from "../constants/events.ts";
 
-if (window.scrollY > 0) {
-  document.documentElement.dataset.scrolled = "true";
-}
-
-window.addEventListener("scroll", () => {
-  const scrolled = window.scrollY > 0;
-  if (scrolled) {
+function setupScroll() {
+  if (window.scrollY > 0) {
     document.documentElement.dataset.scrolled = "true";
-  } else {
-    delete document.documentElement.dataset.scrolled;
   }
 
-  document.documentElement.dispatchEvent(new ScrollStateEvent(scrolled));
-});
+  window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY > 0;
+    if (scrolled) {
+      document.documentElement.dataset.scrolled = "true";
+    } else {
+      delete document.documentElement.dataset.scrolled;
+    }
+
+    document.documentElement.dispatchEvent(new ScrollStateEvent(scrolled));
+  });
+}
+
+if (typeof window !== "undefined") {
+  setupScroll();
+}
