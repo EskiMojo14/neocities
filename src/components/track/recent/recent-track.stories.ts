8fe090571aa6c6638f429431dd9981212495f479
Ben Durrant <ben.durrant@marketdojo.com>
2025-07-04 11:51:35 +0100

    create recent-tracks component with fetching task

--- src/components/track/recent/recent-track.stories.ts
+++ src/components/track/recent/recent-track.stories.ts
@@ -1,6 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
-import { html } from "lit";
-import { repeat } from "lit/directives/repeat.js";
 import "./recent-track.ts";
 import "../track-list.css";
 
@@ -8,25 +6,6 @@ const meta = {
   title: "Components/Track/Recent",
   tags: ["autodocs"],
   component: "recent-track",
-  render: (args) => html`
-    <h4 class="headline6">Recently played</h4>
-    <ol class="track-list">
-      ${repeat(
-        Array.from({ length: 6 }, (_, i) => i),
-        (i) => i,
-        () =>
-          html`<recent-track
-              role="listitem"
-              .artist=${args.artist}
-              .name=${args.name}
-              .thumbnail=${args.thumbnail}
-              .date=${args.date}
-              .nowPlaying=${args.nowPlaying}
-            ></recent-track>
-            <hr class="inset" />`,
-      )}
-    </ol>
-  `,
   args: {
     artist: "Dirty Loops",
     name: "When The Time Is Right",
---
+++ src/components/track/recent/recent-tracks.ts
@@ -0,0 +1,60 @@
+import { Task } from "@lit/task";
+import { html, LitElement, unsafeCSS } from "lit";
+import { customElement } from "lit/decorators.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import { repeat } from "lit/directives/repeat.js";
+import { when } from "lit/directives/when.js";
+import { getRecentTracks } from "../../../data/lastfm.ts";
+import base from "../../../styles/utility/baseline.css?type=raw";
+import "../../spinner/spinner.ts";
+import trackList from "../track-list.css?type=raw";
+import "./recent-track.ts";
+
+@customElement("recent-tracks")
+export default class RecentTracks extends LitElement {
+  static styles = [unsafeCSS(base), unsafeCSS(trackList)];
+
+  #fetchTracks = new Task(this, {
+    args: () => [],
+    // limit doesn't include now playing track, so might be 6 if current track is now playing
+    task: async (_, { signal }) => getRecentTracks(5, { signal }),
+  });
+
+  render() {
+    return html`
+      <h4 class="headline6">Recently played</h4>
+      <ol class="track-list">
+        ${this.#fetchTracks.render({
+          pending: () => html`<hourglass-spinner></hourglass-spinner>`,
+          complete: (tracks) =>
+            repeat(
+              tracks,
+              (track) => track.name,
+              (track, index) =>
+                html`<recent-track
+                    role="listitem"
+                    artist=${track.artist}
+                    album=${track.album}
+                    name=${track.name}
+                    thumbnail=${ifDefined(track.image.large)}
+                    date=${ifDefined(track.date)}
+                    .nowPlaying=${track.nowPlaying}
+                  ></recent-track>
+                  ${when(
+                    index < tracks.length - 1,
+                    () => html`<hr class="inset" />`,
+                  )}`,
+            ),
+          error: () =>
+            html`<p class="error body2">Failed to load recent tracks</p>`,
+        })}
+      </ol>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "recent-tracks": RecentTracks;
+  }
+}
