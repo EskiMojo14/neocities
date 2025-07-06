60fc2e2d582645b5a5c7655946ee73f0d24d4d80
Ben Durrant <ben.durrant@marketdojo.com>
2025-07-06 01:06:43 +0100

    add average scrobble count

---
+++ src/components/lastfm/daily-avg-scrobbles.ts
@@ -0,0 +1,44 @@
+import { html, LitElement, nothing, unsafeCSS } from "lit";
+import { customElement } from "lit/decorators.js";
+import { QueryController } from "../../controllers/query-controller.ts";
+import { getUserData } from "../../data/lastfm.ts";
+import { withStyle } from "../../mixins/page-style.ts";
+import base from "../../styles/utility/baseline.css?type=raw";
+import { decimalFormat } from "../../utils/index.ts";
+import "../spinner/spinner.ts";
+
+const startDate = new Date("2017-09-22");
+
+@customElement("daily-avg-scrobbles")
+export default class DailyAvgScrobbles extends withStyle(LitElement) {
+  static styles = [unsafeCSS(base)];
+
+  calculateDailyAvg(playcount: number) {
+    return Math.round(
+      playcount / ((Date.now() - startDate.getTime()) / (1000 * 60 * 60 * 24)),
+    );
+  }
+
+  #fetchPlaycount = new QueryController(this, () => ({
+    ...getUserData(),
+    select: (data) => this.calculateDailyAvg(data.playcount),
+  }));
+
+  render(): unknown {
+    if (typeof window === "undefined") return nothing;
+    return this.#fetchPlaycount.render({
+      success: ({ data }) =>
+        html`(an average of
+          <span aria-label=${decimalFormat(data, "normal")}>
+            ${decimalFormat(data, this.pageStyle)}
+          </span>
+          scrobbles per day)`,
+    });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "daily-avg-scrobbles": DailyAvgScrobbles;
+  }
+}
