763012b4bad56e3c3377b1eac0960c623117ab64
Ben Durrant <ben.durrant@marketdojo.com>
2025-07-11 11:53:11 +0100

    replace fetch-mock with msw

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import { transformRawImports } from "./src/vite/raw-plugin.ts";
 
 export default defineConfig({
+  publicDir: "./src/assets",
   // 5) add it the plugins option
   plugins: [transformRawImports()],
+  define: {
+    "process.env": Object.fromEntries(
+      Object.entries(process.env).filter(([key]) => key.startsWith("LASTFM")),
+    ),
+  },
 });
