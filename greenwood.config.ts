e8e76cbd041aaf16eb486677da761a5fed744a0b
Ben Durrant <ben.durrant@marketdojo.com>
2025-10-23 21:38:28 +0100

    update to greenwood 0.33

--- greenwood.config.ts
+++ greenwood.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@greenwood/cli";
 import { greenwoodPluginImportRaw } from "@greenwood/plugin-import-raw";
 import { greenwoodPluginPostCss } from "@greenwood/plugin-postcss";
 import { greenwoodPluginRendererLit } from "@greenwood/plugin-renderer-lit";
+import { greenwoodPluginMarkdown } from "@greenwood/plugin-markdown";
 import { envPlugin } from "./src/greenwood/env.ts";
 import { tagsPlugin } from "./src/greenwood/tags.ts";
 
@@ -10,14 +11,14 @@ export default {
   activeContent: true,
   isolation: true,
   plugins: [
+    greenwoodPluginMarkdown({
+      plugins: ["@mapbox/rehype-prism"],
+    }),
     greenwoodPluginRendererLit(),
     greenwoodPluginPostCss(),
     greenwoodPluginImportRaw(),
     tagsPlugin(),
     envPlugin(),
   ],
-  markdown: {
-    plugins: ["@mapbox/rehype-prism"],
-  },
   useTsc: true,
 } satisfies Config;
