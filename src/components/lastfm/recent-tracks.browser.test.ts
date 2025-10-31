dea83658eebffb84fd10bbc2c53ad8369d91d010
Ben Durrant <ben.durrant@marketdojo.com>
2025-10-31 10:15:09 +0000

    test for recent tracks

---
+++ src/components/lastfm/recent-tracks.browser.test.ts
@@ -0,0 +1,58 @@
+import { html } from "lit";
+import { delay, HttpResponse } from "msw";
+import { expect } from "vitest";
+import { page } from "vitest/browser";
+import { mockEndpoint } from "../../data/lastfm.mock.ts";
+import { getRecentTracks } from "../../data/lastfm.ts";
+import { searchLinks } from "../../data/music.ts";
+import { it } from "../../vite/utils.browser.ts";
+import "./recent-tracks.ts";
+
+it("should display recent tracks", async ({ worker }) => {
+  worker.use(
+    mockEndpoint(getRecentTracks, async () => {
+      await delay();
+
+      return HttpResponse.json({
+        recenttracks: {
+          track: [
+            {
+              name: "When The Time Is Right",
+              artist: { "#text": "Dirty Loops" },
+              album: { "#text": "When The Time Is Right" },
+              image: [
+                {
+                  size: "large",
+                  "#text":
+                    "https://lastfm.freetls.fastly.net/i/u/174s/14d1fe21a22a2e4eca73353ce613d555.jpg",
+                },
+              ],
+              date: {
+                uts: "1751810987",
+              },
+            },
+          ],
+        },
+      });
+    }),
+  );
+
+  const screen = page.render(html`<recent-tracks></recent-tracks>`);
+
+  await expect.element(screen.getByText("Recently played")).toBeInTheDocument();
+
+  await expect
+    .element(screen.getByText("When The Time Is Right"))
+    .toBeInTheDocument();
+  await expect.element(screen.getByText("Dirty Loops")).toBeInTheDocument();
+
+  for (const link of searchLinks) {
+    await expect
+      .element(
+        screen.getByRole("link", {
+          name: `Search ${link.label} for When The Time Is Right by Dirty Loops`,
+        }),
+      )
+      .toBeInTheDocument();
+  }
+});
